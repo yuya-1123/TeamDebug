fa851f9f24c04fd42a4cc211116ae8af0200d233
yuya <yuya@DESKTOP-8J7GEU4>
2024-08-02 13:00:45 +0900

    プロジェクト ファイルを追加します。

---
+++ WebApplication1/Controllers/KashidasiController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class KashidasiController : Controller
+    {
+
+        private readonly ILogger<KashidasiController> _logger;
+
+        public KashidasiController(ILogger<KashidasiController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Mainmenu()
+        {
+            return View();
+        }
+        public IActionResult Device()
+        {
+            return View();
+        }
+
+        public IActionResult User()
+        {
+            return View();
+        }
+        public IActionResult Rental()
+        {
+            return View();
+        }
+    }
+}
